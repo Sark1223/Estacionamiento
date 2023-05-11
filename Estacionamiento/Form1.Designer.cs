07ee924d6b3b2bec2d879da2ae5e2c71356eddb3
VilladaEdwinMSTR <93624470+VilladaEdwinMSTR@users.noreply.github.com>
2023-05-11 09:51:51 -0700

    123

--- Estacionamiento/Form1.Designer.cs
+++ Estacionamiento/Form1.Designer.cs
@@ -29,12 +29,49 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.pnlTop = new System.Windows.Forms.Panel();
+            this.DrgForma = new ns1.BunifuDragControl(this.components);
+            this.rdForma = new ns1.BunifuElipse(this.components);
+            this.SuspendLayout();
+            // 
+            // pnlTop
+            // 
+            this.pnlTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlTop.Location = new System.Drawing.Point(0, 0);
+            this.pnlTop.Name = "pnlTop";
+            this.pnlTop.Size = new System.Drawing.Size(1187, 46);
+            this.pnlTop.TabIndex = 0;
+            // 
+            // DrgForma
+            // 
+            this.DrgForma.Fixed = true;
+            this.DrgForma.Horizontal = true;
+            this.DrgForma.TargetControl = this.pnlTop;
+            this.DrgForma.Vertical = true;
+            // 
+            // rdForma
+            // 
+            this.rdForma.ElipseRadius = 30;
+            this.rdForma.TargetControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1187, 682);
+            this.Controls.Add(this.pnlTop);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel pnlTop;
+        private ns1.BunifuDragControl DrgForma;
+        private ns1.BunifuElipse rdForma;
     }
 }
 
