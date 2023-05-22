84220456e8221c1b82e2b2f14bfa43657f98b6b1
JorgeGutierrezP <jorgegp20034@gmail.com>
2023-05-22 13:19:43 -0700

    avance peque
    Lineas de cajon seccionadas y picturebox para los carros (falta agregar)

--- Estacionamiento/Form1.Designer.cs
+++ Estacionamiento/Form1.Designer.cs
@@ -30,11 +30,27 @@
         {
             this.components = new System.ComponentModel.Container();
             this.pnlTop = new System.Windows.Forms.Panel();
+            this.bunifuImageButton1 = new ns1.BunifuImageButton();
             this.DrgForma = new ns1.BunifuDragControl(this.components);
             this.rdForma = new ns1.BunifuElipse(this.components);
-            this.bunifuImageButton1 = new ns1.BunifuImageButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.panel9 = new System.Windows.Forms.Panel();
+            this.panel10 = new System.Windows.Forms.Panel();
+            this.carro1 = new System.Windows.Forms.PictureBox();
+            this.carro2 = new System.Windows.Forms.PictureBox();
+            this.carro3 = new System.Windows.Forms.PictureBox();
             this.pnlTop.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.carro1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.carro2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.carro3)).BeginInit();
             this.SuspendLayout();
             // 
             // pnlTop
@@ -43,9 +59,25 @@
             this.pnlTop.Controls.Add(this.bunifuImageButton1);
             this.pnlTop.Dock = System.Windows.Forms.DockStyle.Top;
             this.pnlTop.Location = new System.Drawing.Point(0, 0);
+            this.pnlTop.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pnlTop.Name = "pnlTop";
-            this.pnlTop.Size = new System.Drawing.Size(1187, 46);
+            this.pnlTop.Size = new System.Drawing.Size(1583, 57);
             this.pnlTop.TabIndex = 0;
+            // 
+            // bunifuImageButton1
+            // 
+            this.bunifuImageButton1.BackColor = System.Drawing.Color.Transparent;
+            this.bunifuImageButton1.Image = global::Estacionamiento.Properties.Resources.cancelar__2_;
+            this.bunifuImageButton1.ImageActive = null;
+            this.bunifuImageButton1.Location = new System.Drawing.Point(1519, 11);
+            this.bunifuImageButton1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.bunifuImageButton1.Name = "bunifuImageButton1";
+            this.bunifuImageButton1.Size = new System.Drawing.Size(41, 34);
+            this.bunifuImageButton1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.bunifuImageButton1.TabIndex = 0;
+            this.bunifuImageButton1.TabStop = false;
+            this.bunifuImageButton1.Zoom = 10;
+            this.bunifuImageButton1.Click += new System.EventHandler(this.bunifuImageButton1_Click);
             // 
             // DrgForma
             // 
@@ -59,33 +91,140 @@
             this.rdForma.ElipseRadius = 30;
             this.rdForma.TargetControl = this;
             // 
-            // bunifuImageButton1
+            // panel1
             // 
-            this.bunifuImageButton1.BackColor = System.Drawing.Color.Transparent;
-            this.bunifuImageButton1.Image = global::Estacionamiento.Properties.Resources.cancelar__2_;
-            this.bunifuImageButton1.ImageActive = null;
-            this.bunifuImageButton1.Location = new System.Drawing.Point(1139, 9);
-            this.bunifuImageButton1.Name = "bunifuImageButton1";
-            this.bunifuImageButton1.Size = new System.Drawing.Size(31, 28);
-            this.bunifuImageButton1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.bunifuImageButton1.TabIndex = 0;
-            this.bunifuImageButton1.TabStop = false;
-            this.bunifuImageButton1.Zoom = 10;
-            this.bunifuImageButton1.Click += new System.EventHandler(this.bunifuImageButton1_Click);
+            this.panel1.BackColor = System.Drawing.Color.Yellow;
+            this.panel1.Location = new System.Drawing.Point(1303, 100);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(257, 16);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.Yellow;
+            this.panel5.Location = new System.Drawing.Point(1303, 320);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(257, 16);
+            this.panel5.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Yellow;
+            this.panel2.Location = new System.Drawing.Point(1303, 553);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(257, 16);
+            this.panel2.TabIndex = 2;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Yellow;
+            this.panel3.Location = new System.Drawing.Point(1303, 782);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(257, 16);
+            this.panel3.TabIndex = 2;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.Yellow;
+            this.panel4.Location = new System.Drawing.Point(1547, 370);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(13, 56);
+            this.panel4.TabIndex = 3;
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.Color.Yellow;
+            this.panel6.Location = new System.Drawing.Point(1547, 467);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(13, 56);
+            this.panel6.TabIndex = 4;
+            // 
+            // panel7
+            // 
+            this.panel7.BackColor = System.Drawing.Color.Yellow;
+            this.panel7.Location = new System.Drawing.Point(1547, 599);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(13, 56);
+            this.panel7.TabIndex = 5;
+            // 
+            // panel8
+            // 
+            this.panel8.BackColor = System.Drawing.Color.Yellow;
+            this.panel8.Location = new System.Drawing.Point(1547, 693);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(13, 56);
+            this.panel8.TabIndex = 4;
+            // 
+            // panel9
+            // 
+            this.panel9.BackColor = System.Drawing.Color.Yellow;
+            this.panel9.Location = new System.Drawing.Point(1547, 226);
+            this.panel9.Name = "panel9";
+            this.panel9.Size = new System.Drawing.Size(13, 56);
+            this.panel9.TabIndex = 6;
+            // 
+            // panel10
+            // 
+            this.panel10.BackColor = System.Drawing.Color.Yellow;
+            this.panel10.Location = new System.Drawing.Point(1547, 144);
+            this.panel10.Name = "panel10";
+            this.panel10.Size = new System.Drawing.Size(13, 56);
+            this.panel10.TabIndex = 4;
+            // 
+            // carro1
+            // 
+            this.carro1.Location = new System.Drawing.Point(105, 121);
+            this.carro1.Name = "carro1";
+            this.carro1.Size = new System.Drawing.Size(123, 103);
+            this.carro1.TabIndex = 7;
+            this.carro1.TabStop = false;
+            // 
+            // carro2
+            // 
+            this.carro2.Location = new System.Drawing.Point(105, 269);
+            this.carro2.Name = "carro2";
+            this.carro2.Size = new System.Drawing.Size(123, 103);
+            this.carro2.TabIndex = 8;
+            this.carro2.TabStop = false;
+            // 
+            // carro3
+            // 
+            this.carro3.Location = new System.Drawing.Point(105, 420);
+            this.carro3.Name = "carro3";
+            this.carro3.Size = new System.Drawing.Size(123, 103);
+            this.carro3.TabIndex = 9;
+            this.carro3.TabStop = false;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(55)))), ((int)(((byte)(94)))));
-            this.ClientSize = new System.Drawing.Size(1187, 682);
+            this.ClientSize = new System.Drawing.Size(1583, 839);
+            this.Controls.Add(this.carro3);
+            this.Controls.Add(this.carro2);
+            this.Controls.Add(this.carro1);
+            this.Controls.Add(this.panel10);
+            this.Controls.Add(this.panel9);
+            this.Controls.Add(this.panel8);
+            this.Controls.Add(this.panel7);
+            this.Controls.Add(this.panel6);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.pnlTop);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.pnlTop.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.carro1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.carro2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.carro3)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -96,6 +235,19 @@
         private ns1.BunifuDragControl DrgForma;
         private ns1.BunifuElipse rdForma;
         private ns1.BunifuImageButton bunifuImageButton1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel10;
+        private System.Windows.Forms.Panel panel9;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.PictureBox carro3;
+        private System.Windows.Forms.PictureBox carro2;
+        private System.Windows.Forms.PictureBox carro1;
     }
 }
 
